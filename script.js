ee157f785d3d9790c835336b7345433c3e486274
daviddexe <gitdavidhub@gmail.com>
2025-02-02 13:47:26 -0300

    now finish header

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
+if (hamburger && navLinks) {
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+}
